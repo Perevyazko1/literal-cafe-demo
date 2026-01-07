437c95b687c9dce8cb1292bfe6db7d81a0a9c476
Andrey Perevyazko <110488818+Perevyazko1@users.noreply.github.com>
2026-01-07 15:15:50 +0300

    setting for mobile

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -4,10 +4,9 @@ import {Chocolatier} from '@/widgets/chocolatier'
 import {Bistrot} from '@/widgets/bistrot'
 import {Footer} from '@/widgets/footer'
 import {ZoomParallax} from "@/widgets/zoom-parallax";
-import {motion, useScroll, useTransform} from "framer-motion";
+import { useScroll, useTransform} from "framer-motion";
 import {useEffect, useRef} from "react";
 import Lenis from 'lenis';
-import {SectionScroll} from "@/widgets/section-scroll";
 
 
 export const HomePage = () => {
@@ -29,54 +28,28 @@ export const HomePage = () => {
         requestAnimationFrame(raf)
     }, [])
 
-    const redOpacity = useTransform(scrollYProgress, [0, 0.3], [1, 0])
-    const whiteOpacity = useTransform(scrollYProgress, [0.3, 0.6], [0, 1])
-    const yellowOpacity = useTransform(scrollYProgress, [0.6, 1], [0, 1])
+
 
   return (
-    <div
-
-
-        className="min-h-screen flex flex-col gap-[10vh]"
-    >
-      {/*<Hero />*/}
-
-        <ZoomParallax/>
-        {/*<div className="relative h-[400vh]">*/}
-        {/*    /!* ОДИН sticky *!/*/}
-        {/*    <div className="sticky top-0 h-screen overflow-hidden">*/}
-
-        {/*        /!* СЛОИ *!/*/}
-        {/*        <motion.div style={{ opacity: redOpacity }} className="absolute inset-0 bg-red-900" />*/}
-        {/*        <motion.div style={{ opacity: whiteOpacity }} className="absolute inset-0 bg-white" />*/}
-        {/*        <motion.div style={{ opacity: yellowOpacity }} className="absolute inset-0 bg-yellow-400" />*/}
-
-        {/*    </div>*/}
-        {/*</div>*/}
-        <Patisserie />
-        <Bistrot />
-        <Chocolatier />
-
-        {/*<div ref={container} className="relative h-[300vh]">*/}
-        {/*    /!*<SectionScroll scrollYProgress={scrollYProgress}>*!/*/}
-        {/*    /!*    *!/*/}
-        {/*    /!*    /!*<div className={'h-full bg-red-900'} />*!/*!/*/}
-        {/*    /!*</SectionScroll>*!/*/}
-        {/*    /!*<SectionScroll scrollYProgress={scrollYProgress}>*!/*/}
-        {/*    /!*    *!/*/}
-        {/*    /!*    /!*<div className={'h-full  bg-green-900'} />*!/*!/*/}
-        {/*    /!*</SectionScroll>*!/*/}
-        {/*</div>*/}
-
-
-
-
-
-
-
-      {/*<CocktailBar />*/}
-      {/*<Institution />*/}
-      <Footer />
-    </div>
+    <>
+      {typeof window !== "undefined" && window.innerWidth >= 1024 ? (
+        <div className="min-h-screen flex flex-col gap-[10vh]" ref={container}>
+          <ZoomParallax />
+          <Patisserie />
+          <Bistrot />
+          <Chocolatier />
+          <Footer />
+        </div>
+      ) : (
+        <div className="min-h-screen flex items-center justify-center p-8 bg-gray-100 text-center">
+          <div>
+            <h1 className="text-3xl font-bold mb-4 text-gray-700">Демо доступно только на десктопе</h1>
+            <p className="text-lg text-gray-700">
+              Пожалуйста, откройте сайт на ПК или устройстве с шириной экрана больше 1024px.
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
